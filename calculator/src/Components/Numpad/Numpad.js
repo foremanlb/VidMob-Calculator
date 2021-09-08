166cb12ed89cd1ef42f79318b67f88f2319968d9
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-09-08 11:52:53 -0700

    number pad works

--- calculator/src/Components/Numpad/Numpad.js
+++ calculator/src/Components/Numpad/Numpad.js
@@ -6,9 +6,31 @@ export default function Numpad(props) {
     props.setEquation(props.equation.concat(e.target.value))
   }
 
+  const handleClear = (e) => {
+    e.preventDefault()
+    props.setEquation('')
+  }
+
   return (
     <div>
+      <button onClick={handleClear}>Clear</button>
+      <button value='(' onClick={handleClick}>(</button>
+      <button value=')' onClick={handleClick}>)</button>
       <button value='1' onClick={handleClick}>1</button>
+      <button value='2' onClick={handleClick}>2</button>
+      <button value='3' onClick={handleClick}>3</button>
+      <button value='+' onClick={handleClick}>+</button>
+      <button value='4' onClick={handleClick}>4</button>
+      <button value='5' onClick={handleClick}>5</button>
+      <button value='6' onClick={handleClick}>6</button>
+      <button value='-' onClick={handleClick}>-</button>
+      <button value='7' onClick={handleClick}>7</button>
+      <button value='8' onClick={handleClick}>8</button>
+      <button value='9' onClick={handleClick}>9</button>
+      <button value='*' onClick={handleClick}>*</button>
+      <button value='.' onClick={handleClick}>.</button>
+      <button value='0' onClick={handleClick}>0</button>
+      <button value='/' onClick={handleClick}>/</button>
     </div>
   )
 }
