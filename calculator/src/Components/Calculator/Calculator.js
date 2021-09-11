d2d079b3721804498f6102caba5a50936f9807b9
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-09-10 20:19:10 -0700

    comments and css

--- calculator/src/Components/Calculator/Calculator.js
+++ calculator/src/Components/Calculator/Calculator.js
@@ -8,10 +8,12 @@ export default function Input() {
   const [equation, setEquation] = useState('')
   // useState to control equation inside of the Input
   const [answer, setAnswer] = useState('')
+  // useState to control answer display
 
   const handleChange = (e) => {
     setEquation(`${e.target.value}`)
   }
+  //takes in values from numpad to update equation
 
   const calculate = (e) => {
     e.preventDefault()
@@ -19,52 +21,46 @@ export default function Input() {
     if (newAnswer.search(/[A-Za-z]/) !== -1) {
       setAnswer('Invalid Input')
       return
+    //Immediately stops function from running if equation includes letters.
     } else if (newAnswer.includes('(')) {
+      //Currently only accounts for a single nested equation, need to modify to accommodate multiple nests.
       const startParentheses = newAnswer.indexOf('(')
       const endParentheses = newAnswer.indexOf(')')
       const nestedEquation = newAnswer.slice(startParentheses + 1, endParentheses)
       const nestedAnswer = operations(nestedEquation).toString()
+      //Runs equations for the nested equation first, then will replace all, including (), with answer and run full equation.
       newAnswer = newAnswer.replace(`(${nestedEquation})`, nestedAnswer)
     }
     newAnswer = operations(newAnswer)
     setAnswer(newAnswer)
-
   }
 
   const operations = (newAnswer) => {
     let configured = newAnswer.split('')
+    //used to break down equation into individual pieces, so that it can be re combined to accommodate parseFloat and check for syntax errors.
     for (let i = 0; i < configured.length; i++) {
       while (configured[i] === '.' && isNaN(configured[i - 1]) === false && isNaN(configured[i + 1]) === false) {
         configured[i] = `${configured[i - 1]}${configured[i]}${configured[i + 1]}`
         configured.splice(i + 1, 1)
         configured.splice(i - 1, 1)
       }
-    }
-    for (let i = 0; i < configured.length; i++) {
       while (configured[i] === '.' && isNaN(configured[i + 1]) === false) {
         configured[i] = `${configured[i]}${configured[i + 1]}`
         configured.splice(i + 1, 1)
       }
-    }
-    for (let i = 0; i < configured.length; i++) {
       while (isNaN(configured[i]) === false && isNaN(configured[i + 1]) === false) {
         configured[i] = `${configured[i]}${configured[i + 1]}`
         configured.splice(i + 1, 1)
       }
     }
+
     for (let i = 0; i < configured.length; i++) {
-      while (configured[i] === '-' && isNaN(configured[i + 1]) === false && isNaN(configured[i - 1]) === true && configured[i - 1] !== ')') {
+      if (configured[i] === '-' && isNaN(configured[i + 1]) === false && isNaN(configured[i - 1]) === true && configured[i - 1] !== ')') {
         configured[i] = `${configured[i]}${configured[i + 1]}`
         configured.splice(i + 1, 1)
-        console.log(configured)
       }
     }
-    for (let i = 0; i < configured.length; i++) {
-      if (isNaN(configured[i]) === false) {
-        configured[i] = parseFloat(configured[i])
-        console.log(configured)
-      }
-    }
+
     for (let i = 0; i < configured.length; i++) {
       const l = i + 1
       if (isNaN(configured[i]) === true && isNaN(configured[l]) === true && configured[i].search(/[+*/-]/g) !== -1 && configured[l].search(/[+*/-]/g) !== -1) {
@@ -72,50 +68,59 @@ export default function Input() {
       }
     }
 
-    for (let i = 0; i < configured.length; i++) {
-      if (answer !== 'Syntax Error' && isNaN(configured[i]) === true && configured[i].match(/[*/]/g) !== null) {
-        switch (configured[i]) {
-          case '*':
-            configured[i] = configured[i - 1] * configured[i + 1]
-            configured.splice(i + 1, 1)
-            configured.splice(i - 1, 1)
-            i -= 1
-            break
-          case '/':
-            configured[i] = configured[i - 1] / configured[i + 1]
-            configured.splice(i + 1, 1)
-            configured.splice(i - 1, 1)
-            i -= 1
-            break
-          default:
-            break;
+    if (configured !== 'Syntax Error') {
+      for (let i = 0; i < configured.length; i++) {
+        if (isNaN(configured[i]) === false) {
+          configured[i] = parseFloat(configured[i])
         }
       }
-    }
+    
+      for (let i = 0; i < configured.length; i++) {
+        if (isNaN(configured[i]) === true && configured[i].match(/[*/]/g) !== null) {
+          switch (configured[i]) {
+            case '*':
+              configured[i] = configured[i - 1] * configured[i + 1]
+              configured.splice(i + 1, 1)
+              configured.splice(i - 1, 1)
+              i -= 1
+              break
+            case '/':
+              configured[i] = configured[i - 1] / configured[i + 1]
+              configured.splice(i + 1, 1)
+              configured.splice(i - 1, 1)
+              i -= 1
+              break
+            default:
+              break;
+          }
+        }
+      }
+      //Switch case used to allow either * or / to be run simultaneously from left to right to accommodate order of operations.
 
-    for (let i = 0; i < configured.length; i++) {
-      if (answer !== 'Syntax Error' && isNaN(configured[i]) === true && configured[i].match(/[-+]/g) !== null) {
-        switch (configured[i]) {
-          case '+':
-            configured[i] = configured[i - 1] + configured[i + 1]
-            configured.splice(i + 1, 1)
-            configured.splice(i - 1, 1)
-            i -= 1
-            break
-          case '-':
-            configured[i] = configured[i - 1] - configured[i + 1]
-            configured.splice(i + 1, 1)
-            configured.splice(i - 1, 1)
-            i -= 1
-            break
-          default:
-            break;
+      for (let i = 0; i < configured.length; i++) {
+        if (isNaN(configured[i]) === true && configured[i].match(/[-+]/g) !== null) {
+          switch (configured[i]) {
+            case '+':
+              configured[i] = configured[i - 1] + configured[i + 1]
+              configured.splice(i + 1, 1)
+              configured.splice(i - 1, 1)
+              i -= 1
+              break
+            case '-':
+              configured[i] = configured[i - 1] - configured[i + 1]
+              configured.splice(i + 1, 1)
+              configured.splice(i - 1, 1)
+              i -= 1
+              break
+            default:
+              break;
+          }
         }
       }
+      //Switch case used to allow either + or - to be run simultaneously from left to right to accommodate order of operations.
     }
-    return configured
+      return configured
   }
-
 
   return (
     <form id='calculator' onSubmit={calculate}>
@@ -129,3 +134,4 @@ export default function Input() {
     </form>
   )
 }
+
--- calculator/src/Components/Numpad/Numpad.js
+++ calculator/src/Components/Numpad/Numpad.js
@@ -17,24 +17,24 @@ export default function Numpad(props) {
   return (
     <div id='numpad'>
       <div id='numpad-grid'>
-        <button id='clear' onClick={handleClear}>Clear</button>
-        <button value='(' onClick={handleClick}>(</button>
-        <button value=')' onClick={handleClick}>)</button>
-        <button value='1' onClick={handleClick}>1</button>
-        <button value='2' onClick={handleClick}>2</button>
-        <button value='3' onClick={handleClick}>3</button>
-        <button value='+' onClick={handleClick}>+</button>
-        <button value='4' onClick={handleClick}>4</button>
-        <button value='5' onClick={handleClick}>5</button>
-        <button value='6' onClick={handleClick}>6</button>
-        <button value='-' onClick={handleClick}>-</button>
-        <button value='7' onClick={handleClick}>7</button>
-        <button value='8' onClick={handleClick}>8</button>
-        <button value='9' onClick={handleClick}>9</button>
-        <button value='*' onClick={handleClick}>*</button>
-        <button value='.' onClick={handleClick}>.</button>
-        <button value='0' onClick={handleClick}>0</button>
-        <button id='divide' value='/' onClick={handleClick}>/</button>
+        <button className='numpad-buttons' id='clear' onClick={handleClear}>Clear</button>
+        <button className='numpad-buttons' value='(' onClick={handleClick}>(</button>
+        <button className='numpad-buttons' value=')' onClick={handleClick}>)</button>
+        <button className='numpad-buttons' value='1' onClick={handleClick}>1</button>
+        <button className='numpad-buttons' value='2' onClick={handleClick}>2</button>
+        <button className='numpad-buttons' value='3' onClick={handleClick}>3</button>
+        <button className='numpad-buttons' value='+' onClick={handleClick}>+</button>
+        <button className='numpad-buttons' value='4' onClick={handleClick}>4</button>
+        <button className='numpad-buttons' value='5' onClick={handleClick}>5</button>
+        <button className='numpad-buttons' value='6' onClick={handleClick}>6</button>
+        <button className='numpad-buttons' value='-' onClick={handleClick}>-</button>
+        <button className='numpad-buttons' value='7' onClick={handleClick}>7</button>
+        <button className='numpad-buttons' value='8' onClick={handleClick}>8</button>
+        <button className='numpad-buttons' value='9' onClick={handleClick}>9</button>
+        <button className='numpad-buttons' value='*' onClick={handleClick}>*</button>
+        <button className='numpad-buttons' value='.' onClick={handleClick}>.</button>
+        <button className='numpad-buttons' value='0' onClick={handleClick}>0</button>
+        <button className='numpad-buttons' id='divide' value='/' onClick={handleClick}>/</button>
       </div>
     </div>
   )
